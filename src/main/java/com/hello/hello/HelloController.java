aa72b054c655145cd3278f24b7c22f458d903e55
Ito-0131 <berufegoal@yahoo.co.jp>
2023-09-10 17:52:42 +0900

    {表示される文字をWelcome to RaiseTech！に変更}

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/great")
+    public String great() {
+        return "Welcome to RaiseTech！";
     }
 }
